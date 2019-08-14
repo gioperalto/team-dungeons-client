a4065b871b2a76bf4d0cd36c4b6b3d93adab167f
gioperalto <gyoperalto@gmail.com>
2019-08-14 17:51:25 -0400

    craft: some small changes to dice visibility

--- src/js/components/AppRouter.js
+++ src/js/components/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './Home'
+import CreatePlayer from './CreatePlayer';
 
 class AppRouter extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class AppRouter extends React.Component {
         <Router>
           <div>
             <Route exact path='/' component={Home} />
+            <Route path='/player/create' component={CreatePlayer} />
           </div>
         </Router>
       );
--- src/js/components/CreatePlayer.js
+++ src/js/components/CreatePlayer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import bg1 from '../../static/images/home.jpg';
 import axios from 'axios';
 import '../../css/App.css';
 
@@ -6,63 +7,66 @@ class CreatePlayer extends React.Component {
   constructor () { 
     super()
     this.state = {
+      visibility: {},
       stats: []
     }
 
     this.rollStats = this.rollStats.bind(this);
   }
 
+  hide() {
+      this.setState({ visibility: { display: 'none' } });
+  }
+
   rollStats () {
     axios.get('http://localhost:5001/api/v5/players/rollStats')
       .then(response => this.setState({stats: response.data}));
+    this.hide();
   }
 
-  createStats () {
+  createdStats () {
     const stats = [];
 
+    // stats.push(<div class='col s12'><h5 class='header'>Here are the results of your roll:</h5></div>);
+
+    // stats.push(<div class='col s3'></div>);
     for(let i = 0; i < this.state.stats.length; i++) {
-      stats.push(<div class='square'>{this.state.stats[i]}</div>);
+      stats.push(<div class='col s1'><h5 class='header'>{this.state.stats[i]}</h5></div>);
     }
+    // stats.push(<div class='col s3'></div>);
 
     return stats;
   }
   
   render () { 
     return (
-      <div className="CreatePlayer">
-        <div className='hero'>
-          <h1 className='title'>Team Dungeons</h1>
-          <p className='description'>
-            To get started, roll your stats.
-          </p>
-          <button className='button' onClick={() => this.rollStats()}>
-              Roll
-          </button>
-
-          <div className='dice'>
-            {this.createStats()}
+      <div class="CreatePlayer">
+        <div class='parallax-container overlay'>
+          <div class='section no-pad-bot'>
+            <div class='container'>
+              <h1 class='header center white-text'>Team Dungeons</h1> 
+            </div>
+            <div id='step1' style={this.state.visibility} >
+              <div class='row center'>
+                <h5 class='header col s12 white-text'>
+                  Roll your dice to get started.
+                </h5>
+              </div>
+              <div class='row center'>
+                <button class='btn-large deep-orange' onClick={() => this.rollStats()}>
+                  Roll
+                </button>
+              </div>
+            </div>
+            <div id='step2'>
+              <div class='row center white'>
+                {this.createdStats()}
+              </div>
+            </div>
           </div>
-
-          {/* <div className='row'>
-            <Link href='https://github.com/zeit/next.js#getting-started'>
-              <a className='card'>
-                <h3>Getting Started &rarr;</h3>
-                <p>Learn more about Next.js on GitHub and in their examples</p>
-              </a>
-            </Link>
-            <Link href='https://github.com/zeit/next.js/tree/master/examples'>
-              <a className='card'>
-                <h3>Examples &rarr;</h3>
-                <p>Find other example boilerplates on the Next.js GitHub</p>
-              </a>
-            </Link>
-            <Link href='https://github.com/zeit/next.js'>
-              <a className='card'>
-                <h3>Create Next App &rarr;</h3>
-                <p>Was this tool helpful? Let us know how we can improve it!</p>
-              </a>
-            </Link>
-          </div> */}
+          <div class='parallax'>
+              <img src={bg1} alt='Background Top' />
+          </div>
         </div>
       </div>
     );
