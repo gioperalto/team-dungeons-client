59e2d9c4b105dbf61f7fd2dbae146f17b7ea58ff
gioperalto <gyoperalto@gmail.com>
2019-08-14 17:02:52 -0400

    craft: added react router to handle multi-page strategy

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import Menu from './js/components/Menu';
-import Home from './js/components/Home';
+import AppRouter from './js/components/AppRouter';
 import * as serviceWorker from './js/serviceWorker';
 
 ReactDOM.render(<Menu />, document.getElementById('menu'));
-ReactDOM.render(<Home />, document.getElementById('home'));
+ReactDOM.render(<AppRouter />, document.getElementById('page'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
